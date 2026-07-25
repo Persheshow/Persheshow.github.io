97342a6e9bbb1071a89f3b26b655b50a144dfc9d
Persheshow <napolitanolorenzo93@gmail.com>
2026-07-25 20:14:49 +0200

    chore: remove outdated documentation and license files; update project name in package-lock and config

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Lorenzo Napolitano",
+  title: "Junior Software Engineer",
+  description: "Portfolio website of Lorenzo Napolitano",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "napolitanolorenzo39@gmail.com",
+    linkedin: "https://www.linkedin.com/in/lorenzo-napolitano-439271275/",
+    //twitter: "https://x.com/rfitzio",
+    github: "https://github.com/Persheshow",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
